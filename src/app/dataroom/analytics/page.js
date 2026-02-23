f96b6cec0a7e1b895187d9bad0e26ac2e88fbe98
Robinkumar J <robin246j@gmail.com>
2026-02-23 13:56:16 +0530

    Data room v1 - build errors solving v1

--- src/app/dataroom/analytics/page.js
+++ src/app/dataroom/analytics/page.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import DataRoomNav from '../../components/dataroom/DataRoomNav';
 import { useRouter } from 'next/navigation';
 import {
   ArrowLeft,
